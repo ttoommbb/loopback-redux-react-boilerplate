0d5dd92bec5c80b5915d2820180f2b19a481cb65
Bohong <ttoommbb@126.com>
2018-08-22 13:09:06 +0800

    Upgrade dependencies and fix webpack prod conf
    Use yarn-upgrade-all to upgrade dependencies,
    fix webpack prod conf since 3 not compatible with 4,
    ref https://webpack.js.org/configuration/
    ref https://webpack.js.org/migrate/4/

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -2,32 +2,36 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: [
-    './client/index'
-  ],
-  output: {
-    path: path.join(__dirname, '.build/dist'),
-    filename: 'bundle.js',
-    publicPath: '/.build'
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel-loader'],
-      exclude: /node_modules/,
-      include: path.join(__dirname, 'client')
+    entry: [
+        './client/index'
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.[jt]sx?$/,
+                include: [path.resolve(__dirname, 'client')],
+                exclude: [path.resolve(__dirname, '/node_modules/')],
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {loader: 'style-loader'},
+                    {loader: 'css-loader'}
+                ]
+
+            }]
     },
-    {
-      test: /\.css$/,
-      use: [
-        { loader: "style-loader" },
-        { loader: "css-loader" }
-      ]
-    }]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-  ]
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.ts', '.tsx']
+    },
+    output: {
+        path: path.join(__dirname, '.build/dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    plugins: [
+
+    ],
+    mode: 'production'
 };
